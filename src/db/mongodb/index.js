f259779c7ae842667b8262622a9573706022434a
luisfuentech <luisfuentes.au@gmail.com>
2024-06-24 21:47:02 -0500

    feat: create function to retry connection with mongoose

--- src/db/mongodb/index.js
+++ src/db/mongodb/index.js
@@ -3,13 +3,22 @@ import mongoose from 'mongoose';
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASS = process.env.MONGO_PASS;
 const MONGO_HOST = process.env.MONGO_HOST; // mymongodb // Replace it with the name of the mongo container
+const DOCKER_MONGO_PORT = process.env.DOCKER_MONGO_PORT;
 
-mongoose.connect(
-  `mongodb://${MONGO_USERNAME}:${MONGO_PASS}@${MONGO_HOST}:27017/pet?authSource=admin`
-);
+const connectWithRetry = () => {
+  mongoose
+    .connect(
+      `mongodb://${MONGO_USERNAME}:${MONGO_PASS}@${MONGO_HOST}:${DOCKER_MONGO_PORT}/pets_store?authSource=admin`
+    )
+    .catch((error) => {
+      console.error(`Can't connect to mongodb ->`, error);
+
+      setTimeout(connectWithRetry, 5000);
+    });
+};
 
 mongoose.connection.on('connected', () => {
-  console.log('mongodb successfully connected');
+  console.log('successfully connected to mongodb');
 });
 
 mongoose.connection.on('disconnected', () => {
@@ -17,7 +26,7 @@ mongoose.connection.on('disconnected', () => {
 });
 
 mongoose.connection.on('error', (error) => {
-  console.log(`Error connecting to mongodb -> ${error?.message}`);
+  console.log(`Error connecting to mongodb`, error);
 });
 
-export { mongoose };
+export { mongoose, connectWithRetry };
